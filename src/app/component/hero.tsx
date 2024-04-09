317c293dd07034e8b061fe6bd3cb2a890d949002
yoseph fernando <yoseph@iykra.com>
2024-04-09 16:46:10 +0700

    chage profile pic path

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Hero(){
     return (
         <div className="flex flex-col p-4 sm:p-8 bg-cyan-500 w-full items-center justify-center h-1/2">
-            <div className="w-[10rem] h-[10rem] bg-cover bg-no-repeat bg-[url('/ysp.jpg')] bg-center rounded-full"></div>
+            <div className="w-[10rem] h-[10rem] bg-cover bg-no-repeat bg-[url('/resume/ysp.jpg')] bg-center rounded-full"></div>
             <p className="font-tilt-neon text-[30px]">Herman Yoseph Fernando</p>
             <p className="font-tilt-neon my-7 text-[24px]">Full stack devloper</p>
             <div className="flex gap-3 justify-center">
